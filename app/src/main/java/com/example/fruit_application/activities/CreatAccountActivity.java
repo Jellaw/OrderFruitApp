2592b1f9469105a16804d7e6bea1fb76067b0599
nphat24 <106876162+nphat24@users.noreply.github.com>
2023-08-17 08:45:30 +0700

    Welcome

--- app/src/main/java/com/example/fruit_application/activities/CreatAccountActivity.java
+++ app/src/main/java/com/example/fruit_application/activities/CreatAccountActivity.java
@@ -3,6 +3,7 @@ package com.example.fruit_application.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,23 +16,36 @@ public class CreatAccountActivity extends AppCompatActivity {
     Button btnCreatAcc , btnBacktoSigin;
     EditText edtFullname , edtEmailCA , edtPwCA;
     TextView tvBackSignin;
+    String emailCA , fullnamCA , passwordCA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_account);
 
         btnCreatAcc = findViewById(R.id.btnCreateAcc);
-        btnBacktoSigin = findViewById(R.id.btnBackSignin);
+        btnBacktoSigin = findViewById(R.id.btnBackWc);
         edtEmailCA = findViewById(R.id.edtEmailCreatAcc);
         edtFullname = findViewById(R.id.edtFullnameCreatAcc);
         edtPwCA = findViewById(R.id.edtPasswordCreatAcc);
         tvBackSignin = findViewById(R.id.tvBacktoSignin);
 
+
         btnCreatAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CreatAccountActivity.this, SigninActivity.class);
-                startActivity(intent);
+                emailCA = edtEmailCA.getText().toString().trim();
+                fullnamCA = edtFullname.getText().toString().trim();
+                passwordCA = edtPwCA.getText().toString().trim();
+
+                if (emailCA.isEmpty() || fullnamCA.isEmpty() || passwordCA.isEmpty()){
+                    edtEmailCA.setError("Vui lòng nhập đủ thông tin");
+                    edtFullname.setError("Vui lòng nhập đủ thông tin");
+                    edtPwCA.setError("Vui lòng nhập đủ thông tin");
+                }else {
+
+                    Intent intent = new Intent(CreatAccountActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                }
             }
         });
         tvBackSignin.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/fruit_application/activities/SigninActivity.java
+++ app/src/main/java/com/example/fruit_application/activities/SigninActivity.java
@@ -3,6 +3,7 @@ package com.example.fruit_application.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,12 +28,22 @@ public class SigninActivity extends AppCompatActivity {
         edtEmailSignin = findViewById(R.id.edtEmailSiginin);
         edtPwSigin = findViewById(R.id.edtPasswordSignin);
 
-        email = edtEmailSignin.getText().toString().trim();
-        password = edtPwSigin.getText().toString().trim();
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                email = edtEmailSignin.getText().toString().trim();
+                password = edtPwSigin.getText().toString().trim();
 
+
+
+                if (email.isEmpty() || password.isEmpty()){
+                    edtEmailSignin.setError("Vui lòng nhập đủ thông tin");
+                    edtPwSigin.setError("Vui lòng nhập đủ thông tin");
+                }else {
+
+                    Intent intent = new Intent(SigninActivity.this, WelcomeActivity.class);
+                    startActivity(intent);
+                }
             }
         });
         tvCreatAcc.setOnClickListener(new View.OnClickListener() {
